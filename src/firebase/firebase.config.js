0aa29efdd80a0c974a3b7ffcb1f0b2c0aef11748
SagorKhan-Dev <sagorkhan.cit@gmail.com>
2024-05-05 18:01:35 +0600

    created project with necessery packages and design login, register, navbar, footer and in home page a carousel and tabCategories. all auth related work done.

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,13 @@
+
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyCi9WcqBJSIP-1LRxr6sgy1lMOd9-umYb4",
+  authDomain: "solosphere-64340.firebaseapp.com",
+  projectId: "solosphere-64340",
+  storageBucket: "solosphere-64340.appspot.com",
+  messagingSenderId: "828795207953",
+  appId: "1:828795207953:web:baeace2fb2ca943f614cb0"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
---
+++ src/routes/Routes.jsx
@@ -0,0 +1,26 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layouts/Main";
+import Home from "../pages/Home";
+import Login from "../pages/Authentication/Login";
+import Registration from "../pages/Authentication/Registration";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/registration",
+        element: <Registration />,
+      },
+    ],
+  },
+]);
