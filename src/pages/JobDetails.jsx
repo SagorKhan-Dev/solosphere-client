22ccd4165fcc4380c56e95fa650298834a0df5d8
SagorKhan-Dev <sagorkhan.cit@gmail.com>
2024-05-07 00:30:14 +0600

    fixing date and buyer information in jobDetails page

--- src/pages/JobDetails.jsx
+++ src/pages/JobDetails.jsx
@@ -18,12 +18,13 @@ const JobDetails = () => {
     deadline,
     min_price,
     max_price,
-    buyer_email,
+    buyer,
   } = job || {};
 
   const handleFormSubmission = async (e) => {
     e.preventDefault();
-    if (user?.email === buyer_email) return toast.error("Action not permitted");
+    if (user?.email === buyer?.email)
+      return toast.error("Action not permitted");
 
     const form = e.target;
     const jobId = _id;
@@ -46,7 +47,8 @@ const JobDetails = () => {
       job_title,
       category,
       status,
-      buyer_email,
+      buyer_email: buyer?.email,
+      buyer,
       email,
     };
 
@@ -67,7 +69,7 @@ const JobDetails = () => {
       <div className="flex-1  px-4 py-7 bg-white rounded-md shadow-md md:min-h-[350px]">
         <div className="flex items-center justify-between">
           <span className="text-sm font-light text-gray-800 ">
-            Deadline: 12/08/2024
+            Deadline: {new Date(deadline).toLocaleDateString()}
           </span>
           <span className="px-4 py-1 text-xs text-blue-800 uppercase bg-blue-200 rounded-full ">
             {category}
@@ -85,13 +87,15 @@ const JobDetails = () => {
           </p>
           <div className="flex items-center gap-5">
             <div>
-              <p className="mt-2 text-sm  text-gray-600 ">Name: Jhankar Vai.</p>
               <p className="mt-2 text-sm  text-gray-600 ">
-                Email: jhankar@mahbub.com
+                Name: {buyer?.name}
+              </p>
+              <p className="mt-2 text-sm  text-gray-600 ">
+                Email: {buyer?.email}
               </p>
             </div>
             <div className="rounded-full object-cover overflow-hidden w-14 h-14">
-              <img src="" alt="" />
+              <img src={buyer?.photo} alt="" />
             </div>
           </div>
           <p className="mt-6 text-lg font-bold text-gray-600 ">
